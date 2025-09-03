ed611bdb5bb0c02d202d5614d87d87562824d90d
Rafael C Varela <rafael.varela2220@gmail.com>
2025-09-03 07:34:51 -0300

    Dia 6 - Completo

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,11 +1,6 @@
 @props(['active' => false])
 
-@php
- if (htmlspecialchars('<a>')):
-    <a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
-       aria-current="{{ $active ? 'page': 'false' }}"
-        {{ $attributes }}
-    >{{ $slot }}</a>
- else:
-    <button></button>
-@endphp
+<a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="{{ $active ? 'page': 'false' }}"
+    {{ $attributes }}
+>{{ $slot }}</a>
